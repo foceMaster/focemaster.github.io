2213616dce32c13e9695863a753dd430ea868530
Tigerninja <67224884+foceMaster@users.noreply.github.com>
2024-08-07 12:13:32 +0200

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// script.js
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('myButton').addEventListener('click', function() {
-        alert('Button was clicked!');
-        alert("SFFFWFH!");
-    });
-});
